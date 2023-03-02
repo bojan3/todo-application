1abfa6f69449b89af02820710b36dac0b060d625
bojan3 <bojanskokicxd@outlook.com>
2023-03-02 16:14:41 +0100

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoApplication.Models;
+
+namespace ToDoApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ToDoApplication.Models.Task> Task { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoApplication.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Titile { get; set; }
+        public string Description { get; set; }
+
+        public Task()
+        {
+
+        }
+    }
+}
